3278712995bfbcf19c04d884fdb0596447b0b27e
hmlee0228 <leehymy98@naver.com>
2023-10-29 22:10:21 +0900

    버튼 수정

--- src/components/UI/atoms/Input/Button/index.jsx
+++ src/components/UI/atoms/Input/Button/index.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 function Button({ children, type = 'button', className = '', onClick }) {
-  const defaultClasses = 'flex items-center justify-center w-full h-8 rounded-2xl cursor-pointer shadow-lg hover:shadow-xl';
+  const defaultClasses = 'flex items-center justify-center h-8 rounded-2xl cursor-pointer shadow-lg hover:shadow-xl';
 
   const colorClasses = className.includes('bg-white') ? '' : 'bg-blue-400 hover:bg-blue-600 text-white';
 
