bff2329441f845047c4bbd4bc91b32c4a1fac07b
[Minyoung <[leemy2200@gmail.com]>
2023-11-03 18:28:15 +0900

    loader css

--- src/components/UI/molecules/Solutions/index.jsx
+++ src/components/UI/molecules/Solutions/index.jsx
@@ -1,5 +1,6 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
+import InfiniteScroll from 'react-infinite-scroll-component';
 import { addTab } from '../../../../store/tabState';
 import { setSolutionsData } from '../../../../store/SolutionsSlice';
 import SolutionsData from '../../../../../public/api/SolutionsData.json';
@@ -7,12 +8,13 @@ import ListItem from '../ListItem';
 
 export default function index() {
   const dispatch = useDispatch();
+  const solutions = useSelector((state) => state.solutions.solutions);
+  const tabs = useSelector((state) => state.tabs.tabs);
+  const [hasMore, setHasMore] = useState(true);
+
   useEffect(() => {
     dispatch(setSolutionsData(SolutionsData));
   }, [dispatch]);
-
-  const tests = useSelector((state) => state.solutions.solutions);
-  const tabs = useSelector((state) => state.tabs.tabs);
 
   const handleSolutionClick = (solution) => {
     const TabExisting = tabs.some((tab) => tab.id === solution.id);
@@ -20,18 +22,33 @@ export default function index() {
       dispatch(addTab({ id: solution.id, type: 'Post', name: solution.title }));
     }
   };
+  const fetchMoreData = () => {};
+
   return (
     <div className=" px-4 py-4">
       <div className="w-full border flex ">+풀이 공유</div>
-      {tests.map((test) => (
-        <ListItem
-          key={test.solution.id}
-          avatar={test.author.avatar}
-          nickname={test.author.nickname}
-          title={test.solution.title}
-          onClick={() => handleSolutionClick(test.solution)}
-        />
-      ))}
+      <InfiniteScroll
+        dataLength={solutions.length}
+        next={fetchMoreData}
+        hasMore={hasMore}
+        loader={
+          <div className="flex space-x-7 justify-center">
+            <div className="w-5 h-5 bg-gray-300 rounded-full"></div>
+            <div className="w-5 h-5 bg-gray-300 rounded-full"></div>
+            <div className="w-5 h-5 bg-gray-300 rounded-full"></div>
+          </div>
+        }
+      >
+        {solutions.map((solution) => (
+          <ListItem
+            key={solution.solution.id}
+            avatar={solution.author.avatar}
+            nickname={solution.author.nickname}
+            title={solution.solution.title}
+            onClick={() => handleSolutionClick(solution.solution)}
+          />
+        ))}
+      </InfiniteScroll>
     </div>
   );
 }
