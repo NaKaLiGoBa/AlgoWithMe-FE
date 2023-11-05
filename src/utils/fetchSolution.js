583b9ce4b89ac05f314d81497b55c5649b54f62c
hmlee0228 <leehymy98@naver.com>
2023-11-06 07:40:23 +0900

    authToken 추가

--- src/utils/fetchSolution.js
+++ src/utils/fetchSolution.js
@@ -1,10 +1,28 @@
 import axios from 'axios';
 import { localHostURL } from './apiConfig';
 
-const fetchSolution = async (problemId, solutionId) => {
+export const fetchSolution = async (problemId, solutionId) => {
   const url = `${localHostURL}/problems/${problemId}/solutions/${solutionId}`;
   const response = await axios.get(url);
   return response.data;
 };
 
-export default fetchSolution;
+export const updateSolution = async (problemId, solutionId, solutionData, authToken) => {
+  const url = `${localHostURL}/problems/${problemId}/solutions/${solutionId}`;
+  const response = await axios.put(url, solutionData, {
+    headers: {
+      Authorization: `Bearer ${authToken}`
+    }
+  });
+  return response.data;
+};
+
+export const deleteSolution = async (problemId, solutionId, authToken) => {
+  const url = `${localHostURL}/problems/${problemId}/solutions/${solutionId}`;
+  const response = await axios.delete(url, {
+    headers: {
+      Authorization: `Bearer ${authToken}`
+    }
+  });
+  return response.data;
+};
