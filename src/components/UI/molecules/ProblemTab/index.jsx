c8146ce70796322db026f48ef687e25691f19c58
hmlee0228 <leehymy98@naver.com>
2023-11-06 03:08:45 +0900

    react-beautiful-dnd 추가

--- src/components/UI/molecules/ProblemTab/index.jsx
+++ src/components/UI/molecules/ProblemTab/index.jsx
@@ -1,6 +1,12 @@
 import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { setActiveTab } from '../../../../store/tabState';
+import { DragDropContext, Droppable, Draggable } from 'react-beautiful-dnd';
+import {
+  addTab,
+  setActiveTab,
+  reorderTabs,
+  updateTabContent,
+} from '../../../../store/tabState';
 
 export default function index() {
   const dispatch = useDispatch();
@@ -10,38 +16,78 @@ export default function index() {
   const tabs = useSelector((state) => state.tabs.tabs);
   const activeTab = useSelector((state) => state.tabs.activeTab);
   console.log('e', tabs);
-  return (
-    <div>
-      <ul className="flex">
-        {tabs.map((tab) => (
-          <li
-            key={tab.id}
-            className={`py-2 px-4 ${
-              activeTab.id === tab.id
-                ? 'bg-blue-500 text-white'
-                : 'bg-white text-black'
-            }`}
-            onClick={() => handleTabClick(tab)}
-          >
-            {tab.name}
-          </li>
-        ))}
-      </ul>
 
-      {/* {tabs.map((tab) => (
-        <button
-          type="button"
-          key={tab.id}
-          className={`py-2 px-4 ${
-            activeTab.id === tab.id
-              ? 'bg-blue-500 text-white'
-              : 'bg-white text-black'
-          }`}
-          onClick={() => handleTabClick(tab)}
-        >
-          {tab.type}
-        </button>
-      ))} */}
-    </div>
+  const handleSolutionClick = (solution) => {
+    const existingTab = tabs.find((tab) => tab.id === solution.id);
+    if (existingTab) {
+      dispatch(setActiveTab(existingTab));
+    } else {
+      const newTab = { id: solution.id, type: 'Post', name: solution.title };
+      dispatch(addTab(newTab));
+      dispatch(setActiveTab(newTab));
+    }
+  };
+
+  // 드래그 앤 드롭
+  const onDragEnd = (result) => {
+    const { destination, source } = result;
+
+    if (!destination) {
+      return;
+    }
+
+    if (
+      destination.droppableId === source.droppableId &&
+      destination.index === source.index
+    ) {
+      return;
+    }
+
+    // tabs 배열 순서를 저장
+    dispatch(
+      reorderTabs({
+        fromIndex: source.index,
+        toIndex: destination.index,
+      }),
+    );
+  };
+
+  return (
+    <DragDropContext onDragEnd={onDragEnd}>
+      <Droppable droppableId="tabs">
+        {(provided) => (
+          <ul
+            className="flex"
+            {...provided.droppableProps}
+            ref={provided.innerRef}
+          >
+            {tabs.map((tab, index) => (
+              <Draggable
+                key={tab.id}
+                draggableId={String(tab.id)}
+                index={index}
+              >
+                {(provided) => (
+                  <li
+                    ref={provided.innerRef}
+                    {...provided.draggableProps}
+                    {...provided.dragHandleProps}
+                    className={`py-2 px-4 ${
+                      activeTab.id === tab.id
+                        ? 'bg-blue-500 text-white'
+                        : 'bg-white text-black'
+                    }`}
+                    onClick={() => handleTabClick(tab)}
+                  >
+                    {tab.name}
+                  </li>
+                )}
+              </Draggable>
+            ))}
+            {provided.placeholder}
+          </ul>
+        )}
+      </Droppable>
+    </DragDropContext>
   );
 }
