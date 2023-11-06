9dba36ed752a5d14e38300d88367b0b4c84e84e4
Gukin Han <gukin.dev@gmail.com>
2023-11-06 16:47:00 +0900

    add

--- src/utils/apiConfig.js
+++ src/utils/apiConfig.js
@@ -1,4 +1,4 @@
 // const localHostURL = 'http://127.0.0.1:8080';
-const localHostURL = 'https://k881facf0dd88a.user-app.krampoline.com/';
+const localHostURL = 'https://k881facf0dd88a.user-app.krampoline.com';
 
 export { localHostURL };
