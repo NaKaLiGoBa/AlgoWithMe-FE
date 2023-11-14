6ab0555215c43a4c81fe3d0d23641e5863c1d057
hmlee0228 <leehymy98@naver.com>
2023-11-14 14:16:42 +0900

    좋아요, 토큰 추가

--- src/utils/fetchSolution.js
+++ src/utils/fetchSolution.js
@@ -3,9 +3,13 @@ import { localHostURL } from './apiConfig';
 
 const endPoint = `${localHostURL}/api/v1/problems/`;
 
-export const fetchSolution = async (problemId, solutionId) => {
+export const fetchSolution = async (problemId, solutionId, authToken) => {
   const url = `${endPoint}${problemId}/solutions/${solutionId}`;
-  const response = await axios.get(url);
+  const response = await axios.get(url, {
+    headers: {
+      Authorization: `Bearer ${authToken}`,
+    },
+  });
   return response.data;
 };
 
@@ -32,4 +36,18 @@ export const deleteSolution = async (problemId, solutionId, authToken) => {
     },
   });
   return response.data;
+};
+
+export const likeSolution = async (problemId, solutionId, authToken) => {
+  const url = `${endPoint}${problemId}/solutions/${solutionId}/like`;
+  try {
+    const response = await axios.post(url, {}, {
+      headers: {
+        Authorization: `Bearer ${authToken}`,
+      },
+    });
+    return response.data;
+  } catch (error) {
+    throw error;
+  }
 };
