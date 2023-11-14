b4500f62c036b6162b98bb005bb1c468346905bc
hmlee0228 <leehymy98@naver.com>
2023-11-14 14:20:10 +0900

    input기능 수정

--- src/components/UI/atoms/Input/CommentInput/index.jsx
+++ src/components/UI/atoms/Input/CommentInput/index.jsx
@@ -1,11 +1,24 @@
-import React from 'react';
+import React, { useState} from 'react';
 
 function CommentInput({ placeholder, onCancel, onComment }) {
+  const [inputText, setInputText] = useState('');
+
+  const handleInputChange = (e) => {
+    setInputText(e.target.value);
+  };
+
+  const handleCommentClick = () => {
+    onComment(inputText);
+    setInputText('');
+  };
+
   return (
     <div className="bg-gray-800 p-4 mt-4 rounded-lg justify-between items-center">
       <textarea
         className="bg-gray-700 text-white w-full p-3 rounded-lg flex-1"
         placeholder={placeholder}
+        value={inputText}
+        onChange={handleInputChange}
       />
       <div className="flex justify-end mt-3">
         <button
@@ -15,7 +28,7 @@ function CommentInput({ placeholder, onCancel, onComment }) {
           Cancel
         </button>
         <button
-          onClick={onComment}
+          onClick={handleCommentClick}
           className="bg-green-500 hover:bg-green-400 px-3 py-1 rounded-full text-m"
         >
           Comment
