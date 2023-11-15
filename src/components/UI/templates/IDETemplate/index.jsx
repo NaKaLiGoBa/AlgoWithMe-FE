675dbfbb3d79887ae3c0e782992b3356c46cc518
Gukin Han <gukin.dev@gmail.com>
2023-11-15 18:05:52 +0900

    refactor: idepage 템플릿 모듈 생성

--- src/components/UI/templates/IDETemplate/index.jsx
+++ src/components/UI/templates/IDETemplate/index.jsx
@@ -1,16 +1,16 @@
 import React from 'react';
-import ProblemPanel from '../molecules/ProblemPanel';
-import Editor from '../molecules/Editor';
-import OutputPanel from '../molecules/OutputPanel';
-import Header from '../molecules/Navigation/Header';
-import ProblemTab from '../molecules/ProblemTab';
-import Solutions from '../molecules/Solutions';
-import SolutionTest from '../molecules/SolutionTest';
+import ProblemPanel from '../../molecules/ProblemPanel';
+import Editor from '../../molecules/Editor';
+import OutputPanel from '../../molecules/OutputPanel';
+import Header from '../../molecules/Navigation/Header';
+import ProblemTab from '../../molecules/ProblemTab';
+import Solutions from '../../molecules/Solutions';
+import SolutionTest from '../../molecules/SolutionTest';
 
 const index = ({ activeTab }) => (
   <div>
     <Header />
-    <main className="flex flex-row bg-[#E7E7E7] h-full">
+    <main className="flex flex-row bg-[#E7E7E7] h-[calc(100%-50px)]">
       <div className="w-[40%] bg-white rounded-xl m-2">
         <ProblemTab />
         <hr />
