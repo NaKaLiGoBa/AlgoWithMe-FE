9781fa44808504ea37da04569b3074bd992b98eb
Gukin Han <gukin.dev@gmail.com>
2023-11-20 15:30:25 +0900

    fix: 이전 문제의 디폴트 코드를 가져오는 에러

--- src/components/pages/IDEPage/index.jsx
+++ src/components/pages/IDEPage/index.jsx
@@ -1,20 +1,18 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import { useSelector, useDispatch } from 'react-redux';
 import { setProblem } from '../../../store/problemSlice';
 import IDETemplate from '../../UI/templates/IDETemplate';
 import { resetTab } from '../../../store/tabState';
 import { toggleChat } from '../../../store/AIChatSlice';
-
-// API
 import getProblemById from '../../../utils/api/v1/problem/getProblemById';
 
-// problem loading
 function ProblemPage() {
   const dispatch = useDispatch();
   const { problemId } = useParams();
   const activeTab = useSelector((state) => state.tabs.activeTab);
   const showChat = useSelector((state) => state.chat.isVisible);
+  const [isDataLoaded, setIsDataLoaded] = useState(false); // Local state to track data loading
 
   useEffect(() => {
     dispatch(resetTab());
@@ -22,12 +20,18 @@ function ProblemPage() {
       .then((response) => response.data)
       .then((data) => {
         dispatch(setProblem(data));
+        setIsDataLoaded(true); // Set to true when data is loaded
       });
   }, [problemId, dispatch]);
 
   const handleChatToggle = () => {
     dispatch(toggleChat());
   };
+
+  // Render IDETemplate only if data is loaded
+  if (!isDataLoaded) {
+    return <div>Loading...</div>; // Or any other loading indicator
+  }
 
   return (
     <IDETemplate
