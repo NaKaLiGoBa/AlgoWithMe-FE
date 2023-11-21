6c793fd14b9b65c3ba50cd2727e173fd4583a665
hmlee0228 <leehymy98@naver.com>
2023-11-21 15:23:43 +0900

    REST_API_KEY 값 수정

--- src/components/UI/molecules/Form/Signin/index.jsx
+++ src/components/UI/molecules/Form/Signin/index.jsx
@@ -80,7 +80,7 @@ function Signin() {
   };
 
   // 카카오 로그인 로직 추가하기
-  const REST_API_KEY = import.meta.env.VITE_REACT_APP_REST_API_KEY;
+  const REST_API_KEY = "ea0d5d44e5604a884f97c30d752b5b90";
   const REDIRECT_URI = `${localHostURL}/auth/signin/kakao`;
   const KAKAO_AUTH_URI = `https://kauth.kakao.com/oauth/authorize?client_id=${REST_API_KEY}&redirect_uri=${REDIRECT_URI}&response_type=code`;
 
