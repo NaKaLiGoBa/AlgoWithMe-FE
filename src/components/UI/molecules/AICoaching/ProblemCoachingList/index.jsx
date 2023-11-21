9b024e3ba229eb58854ba5e8411be3c293ffda9b
[Minyoung <[leemy2200@gmail.com]>
2023-11-21 14:12:55 +0900

    ProblemListFooter를 사용하여 pagination 구현

--- src/components/UI/molecules/AICoaching/ProblemCoachingList/index.jsx
+++ src/components/UI/molecules/AICoaching/ProblemCoachingList/index.jsx
@@ -1,38 +1,60 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-// import problemsData from '../../../../../../public/api/problems.json';
+import ProblemListFooter from '../../Problem/ProblemListFooter';
+import getProblems from '../../../../../utils/api/v1/problem/getProblems';
 
 import {
   nextScreen,
   setSeletedProblemId,
 } from '../../../../../store/AIChatSlice';
+import { setProblems } from '../../../../../store/problemsSlice';
 
 export default function index() {
   const dispatch = useDispatch();
-  // const selectedProblemId = useSelector(
-  //   (state) => state.chat.selectedProblemId,
-  // );
   const problems = useSelector((state) => state.problems.problems);
+  const { totalPages } = useSelector((state) => state.problems);
+  const [page, setPage] = useState(1);
+
+  useEffect(() => {
+    async function fetchData() {
+      const params = { page: page - 1 };
+      const response = await getProblems(params);
+      if (response.success) {
+        dispatch(setProblems(response.data));
+      } else {
+        console.log(response.error);
+      }
+    }
+    fetchData();
+  }, [dispatch, page]);
 
   const problemChatClick = (id) => {
     dispatch(setSeletedProblemId(id));
     dispatch(nextScreen());
   };
 
+  console.log('page', page);
   return (
-    <ul>
-      {problems.map((problem) => (
-        <li
-          key={problem.id}
-          className="p-3 text-lg font-bold border hover:bg-slate-200"
-        >
-          <button type="button" onClick={() => problemChatClick(problem.id)}>
-            {problem.number}
-            {'   '}
-            {problem.title}
-          </button>
-        </li>
-      ))}
-    </ul>
+    <div>
+      <ul>
+        {problems.map((problem) => (
+          <li
+            key={problem.id}
+            className="p-3 text-lg font-bold border hover:bg-slate-200"
+          >
+            <button type="button" onClick={() => problemChatClick(problem.id)}>
+              {problem.number}
+              {'   '}
+              {problem.title}
+            </button>
+          </li>
+        ))}
+      </ul>
+      <ProblemListFooter
+        totalPages={totalPages}
+        currentPage={page}
+        setPage={setPage}
+      />
+    </div>
   );
 }
