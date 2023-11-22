f5f242333e80d7612528de33b661c3ecbeaeca2f
Gukin Han <gukin.dev@gmail.com>
2023-11-22 12:12:26 +0900

    refactor: 문제 상세 페이지 라우터 변경

--- src/App.jsx
+++ src/App.jsx
@@ -20,7 +20,7 @@ function App() {
         <Route path="/" element={<HomePage />} />
         <Route path="/signin" element={<SigninPage />} />
         <Route path="/signup" element={<SignupPage />} />
-        <Route path="/problems/:problemId" element={<IDEPage />} />
+        <Route path="/problems/:problemId/*" element={<IDEPage />} />
         <Route
           path="problems/:problemId/quizzes"
           element={<ProblemQuizzesPage />}
