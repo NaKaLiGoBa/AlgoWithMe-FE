a7e07fb42695c7b9be640be5898c5751baf108fa
Gukin Han <gukin.dev@gmail.com>
2023-11-22 14:36:12 +0900

    fix: 아바타 깨짐 이슈

--- src/components/UI/templates/IDETemplate/index.jsx
+++ src/components/UI/templates/IDETemplate/index.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { useParams, useLocation, Navigate } from 'react-router-dom';
 import ProblemPanel from '../../molecules/ProblemPanel';
 import Editor from '../../molecules/Editor';
 import OutputPanel from '../../molecules/OutputPanel';
@@ -9,35 +10,38 @@ import SolutionTest from '../../molecules/SolutionTest';
 import AIchat from '../../molecules/AICoaching/index';
 import Button from '../../atoms/Input/Button';
 
-const index = ({ activeTab, handleChatToggle, showChat }) => (
-  <div className="h-screen">
-    <Header />
-    <main className="flex flex-row bg-[#E7E7E7] h-[calc(100%-65px)]">
-      <div className="w-[40%] bg-white rounded-xl mt-2 mr-2 h-[100%]">
-        <ProblemTab />
-        <hr />
-        {activeTab.type === 'Description' && <ProblemPanel />}
-        {activeTab.type === 'Solutions' && <Solutions />}
-        {activeTab.type === 'Post' && <SolutionTest />}
-      </div>
-      <div className="grow h-[100%]">
-        <Editor />
-        <OutputPanel />
-      </div>
-      <div className="fixed bottom-4 right-5">
-        {showChat ? (
-          <AIchat handleChatToggle={handleChatToggle} />
-        ) : (
-          <Button
-            className="bg-[#6D4BEB] px-4 py-6 text-lg "
-            onClick={handleChatToggle}
-          >
-            AI
-          </Button>
-        )}
-      </div>
-    </main>
-  </div>
-);
+const index = ({ activeTab, handleChatToggle, showChat }) => {
+  const { problemId, tab, solutionId } = useParams();
+  const location = useLocation();
+
+  if (location.pathname === `/problems/${problemId}`) {
+    return <Navigate to={`/problems/${problemId}/description`} replace />;
+  }
+
+  return (
+    <div className="h-screen">
+      <Header className="bg-transparent" />
+      <main className="flex flex-row bg-[#E7E7E7] h-[calc(100%-70px)] gap-1">
+        <div className="w-[40%] bg-white rounded-xl h-[100%]" />
+        <div className="grow bg-white rounded-xl  h-[100%]">
+          <Editor />
+          <OutputPanel />
+        </div>
+        <div className="fixed bottom-4 right-5">
+          {showChat ? (
+            <AIchat handleChatToggle={handleChatToggle} />
+          ) : (
+            <Button
+              className="bg-[#6D4BEB] px-4 py-6 text-lg "
+              onClick={handleChatToggle}
+            >
+              AI
+            </Button>
+          )}
+        </div>
+      </main>
+    </div>
+  );
+};
 
 export default index;
